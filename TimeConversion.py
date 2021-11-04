6e0a48c44383ff2d2c7b22f7a7d87f46ca723a2d
muhabibi <43407845+muhabibi@users.noreply.github.com>
2021-11-05 07:53:29 +0800

    Create TimeConversion.py

---
+++ TimeConversion.py
@@ -0,0 +1,34 @@
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    f1 = "%I:%M:%S%p"
+    f2 = "%H:%M:%S"
+    s1 = datetime.strptime(s, f1)
+    return s1.strftime("%H:%M:%S")
+    return str(s1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
